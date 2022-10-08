3df6f2abfa8fd2ec224fa15a419c71cb55eac710
Милютин Дмитрий <damilyutin@asuproject.ru>
2022-10-09 03:29:05 +0500

    configurate proj

---
+++ pages/_document.tsx
@@ -0,0 +1,13 @@
+﻿import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+    return (
+        <Html>
+            <Head />
+            <body className='root'>
+                <Main />
+                <NextScript  />
+            </body>
+        </Html>
+    );
+}
