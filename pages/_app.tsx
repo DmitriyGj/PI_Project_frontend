6875afe40c6744046580c33dff38f42ce7ce165f
Милютин Дмитрий <damilyutin@asuproject.ru>
2022-10-09 16:04:09 +0500

    change conf

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import type { AppProps } from 'next/app';
 import '@styles/global.scss';
+import { CacheProvider } from '@emotion/react';
+import createCache from '@emotion/cache';
+
+const cache = createCache({
+    key: 'css',
+    prepend: true,
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    return ( <CacheProvider value={cache}>
+        <Component {...pageProps} />
+    </CacheProvider>);
 }
 
 export default MyApp;
