39926f08d90f26d80a9eb92315630e5be802455e
Kirill Zagul <zagulkirill@gmail.com>
2022-12-05 11:43:35 +0500

    Исправил внешний вид компонента Header согласно дизайну

--- public/src/components/header/Header.tsx
+++ public/src/components/header/Header.tsx
@@ -1,41 +1,48 @@
 import Style from './header.module.scss';
 import { useSelector } from 'react-redux';
-
-import SideBar from '@components/sideBar/SideBar';
-import NavigationMenu from '@components/navigationMenu/NavigationMenu';
-
-import { LinkProps } from '../navigationMenu/types';
-
-import { getCurrentUserRole } from '@store/meetings/selectors';
 import { UserRole } from '@store/users/types';
+import { getCurrentUserRole, getCurrentUser } from '@store/users/selectors';
+import { Button } from 'antd';
 
+import { LinkProps } from '@components/navigationMenu/types';
+
+import NavigationMenu from '@components/navigationMenu/NavigationMenu';
 import Logo from '@components/logo/Logo';
+
 const Header = () => {
     const userStatus = useSelector(getCurrentUserRole);
+    const currentUser = useSelector(getCurrentUser);
 
     const linksInfo: LinkProps[] = [
         {
             id: 1,
             href: '/',
-            label: 'Главная страница'
+            label: 'Главная страница',
         },
         {
             id: 2,
             href: '/test',
-            label: 'Тестовая страница'
+            label: 'Тестовая страница',
         }
     ];
 
     return (
-        <header className={ Style.header }>
-            <Logo width={'200px'} height={'100%'}/>
-            {userStatus == UserRole.ADMIN 
-                ? <>
-                    <NavigationMenu linksInfo={linksInfo}/>  
-                </>
-                : null 
-            }
-            <SideBar width= { '200px' } />
+        <header className={Style.header}>
+            <Logo width={'200px'} height={'100%'} />
+            <div className={Style.headerCols}>
+                <div>
+                    {userStatus == UserRole.ADMIN ? (
+                        <NavigationMenu linksInfo={linksInfo} />
+                    ) : null}
+                </div>
+                <div>
+                    {currentUser? 
+                        <Button style={{ background: '#3D3BBC', borderColor: '#3D3BBC', color: 'white' }}>Выход</Button>
+                        : 
+                        <Button style={{ background: '#3D3BBC', borderColor: '#3D3BBC', color: 'white' }}>Вход</Button>
+                    }
+                </div>
+            </div>
         </header>
     );
 };
